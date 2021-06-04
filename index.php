982450d4d86644cb0bba7342b28eed45b15a73b2
Edwin Acosta <karinate.tellez@gmail.com>
2021-06-03 19:28:24 -0500

    PRUEBA INDEX PHP

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-header("Location: http://example.com/myOtherPage.php%22);
-die();
-?>;
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logotipo.jpg" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="blog.php">Blog</a></li>
+					<li><a href="acerca_de.php">Acerca de</a></li>
+					<li><a href="contacto.php">Contacto</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</body>
