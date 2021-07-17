83694c7a41d32dc26c19e4af095c0ce412a42d58
Edwin Acosta <karinate.tellez@gmail.com>
2021-07-16 20:26:41 -0500

    hoy hicimos más funciones y vimos nuevas funciones

---
+++ funcion parametro/funciones.js
@@ -0,0 +1,12 @@
+function imprimirEnSegundos(){
+    document.getElementById("letrero").innerHTML=document.getElementById("mensaje").value;
+}
+function mostrarDialogo(){
+    alert("hoy es viernes");
+}
+function irAGoogle(){
+    window.open("https://www.google.com");
+}
+function mostrarNombre(){
+    prompt("diga su nombre","karina");
+}
