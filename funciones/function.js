8233a0064888d72a1b5e35d92739d482d9ec1d66
Edwin Acosta <karinate.tellez@gmail.com>
2021-07-20 13:52:02 -0500

    hoy repasamos funciones

---
+++ funciones/function.js
@@ -0,0 +1,5 @@
+function llamarCajita(){
+    // document.getElementById("mostrador").innerHTML=document.getElementById("cajita").value;
+    // document.getElementById("mostrador").innerHTML=document.getElementById("superacion").innerHTML;
+    document.getElementById("mostrador").innerHTML=document.getElementById("peliculasFavoritas").innerHTML;
+}
