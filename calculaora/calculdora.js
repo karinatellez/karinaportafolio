383505fba47ec3d97283b8f0cc34915d714eb901
Edwin Acosta <karinate.tellez@gmail.com>
2021-11-11 18:41:59 -0500

    hoy terminamos  la calculadora

--- calculaora/calculdora.js
+++ calculaora/calculdora.js
@@ -1,68 +1,79 @@
 var operacion = ""; // string
 var estaDigitandoPrimerNumero = true; //booleano
 var primerNumero = "";
-function mostrarNumero(numero) {
-  if (estaDigitandoPrimerNumero == true) {
-    primerNumero += numero;
-    document.getElementById("pantalla").innerHTML = primerNumero;
-  } else {
-    segundoNumero += numero;
-    document.getElementById("pantalla").innerHTML = segundoNumero;
-  }
-}
 var segundoNumero = "";
+var resultado = 0; // numero
+function mostrarNumero(numero) {
+  primerNumero += numero;
+  document.getElementById("pantalla").innerHTML = primerNumero;
+}
 
 function mostrarResultado() {
-  var resultado = 0; // numero
+  if (primerNumero == "") {
+    primerNumero = 0;
+  }
   if (operacion == "sumar") {
-    resultado = parseInt(primerNumero) + parseInt(segundoNumero);
+    resultado += parseFloat(primerNumero);
   }
   if (operacion == "dividir") {
-    resultado = parseInt(primerNumero) / parseInt(segundoNumero);
+    if (resultado != 0) {
+      resultado = resultado / parseFloat(primerNumero);
+    } else {
+      resultado = parseFloat(primerNumero);
+    }
   }
+
   if (operacion == "resta") {
-    resultado = parseInt(primerNumero) - parseInt(segundoNumero);
+    resultado -= parseFloat(primerNumero);
   }
   if (operacion == "multiplicacion") {
-    resultado = parseInt(primerNumero) * parseInt(segundoNumero);
+    if (resultado != 0) {
+      resultado = resultado * parseFloat(primerNumero);
+    } else {
+      resultado = parseFloat(primerNumero);
+    }
   }
   document.getElementById("pantalla").innerHTML = resultado;
-  operacion = "";
-  primerNumero = "";
-  segundoNumero = "";
-  estaDigitandoPrimerNumero = true;
+  /* operacion = ""; */
 }
 
 function sumar() {
-    estaDigitandoPrimerNumero = false;
-    document.getElementById("pantalla").innerHTML = "";
-    operacion = "sumar";
-  }
-  
+  /*     estaDigitandoPrimerNumero = false; */
+  document.getElementById("pantalla").innerHTML = "";
+  operacion = "sumar";
+  mostrarResultado();
+  primerNumero = "";
+}
+
 function dividir() {
-  estaDigitandoPrimerNumero = false;
+  /*   estaDigitandoPrimerNumero = false; */
   document.getElementById("pantalla").innerHTML = "";
   operacion = "dividir";
+  mostrarResultado();
+  primerNumero = "";
 }
 
 function restar() {
-  estaDigitandoPrimerNumero = false;
+  /*   estaDigitandoPrimerNumero = false; */
   document.getElementById("pantalla").innerHTML = "";
   operacion = "resta";
+  mostrarResultado();
+  primerNumero = "";
 }
 
 function multiplicacion() {
-  estaDigitandoPrimerNumero = false;
+  /*   estaDigitandoPrimerNumero = false; */
   document.getElementById("pantalla").innerHTML = "";
   operacion = "multiplicacion";
+  mostrarResultado();
+  primerNumero = "";
 }
 
 function borrar() {
-    document.getElementById("pantalla").innerHTML = "";
-  estaDigitandoPrimerNumero = true;
+  document.getElementById("pantalla").innerHTML = "";
+  /*   estaDigitandoPrimerNumero = true; */
   primerNumero = "";
   operacion = "";
   segundoNumero = "";
-
   console.log("estaborrando");
 }
